fb7583fea7354fc155cce062cffcba11d3c223ba
raihankaiserkhan <raihankaiserkhan@gmail.com>
2022-03-22 14:19:31 -0400

    03/22/22 02:20pm

--- src/hw16/Use_of_isSelect_method.java
+++ src/hw16/Use_of_isSelect_method.java
@@ -19,24 +19,15 @@ public class Use_of_isSelect_method {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.discover.com/");
-		Thread.sleep(5000);
 	}
 
 	@Test(enabled = true, priority = 1)
 	public void checkBoxTest() throws InterruptedException {
 		driver.findElement(By.xpath("//input[@id=\"id-checkbox-content\"]")).click();
-		Thread.sleep(3000);
-		boolean checkBoxSelected = driver.findElement(By.xpath("//input[@id=\"id-checkbox-content\"]")).isSelected();
 		Thread.sleep(2000);
+		boolean checkBoxSelected = driver.findElement(By.xpath("//input[@id=\"id-checkbox-content\"]")).isSelected();
 		System.out.println("Is the check box selected? Ans: " + checkBoxSelected);
 	}
-
-	//@Test(enabled = true, priority = 2)
-//	public void forgotPasswordTest() throws InterruptedException {
-	//	boolean forgotButtonEnabled = driver.findElement(By.xpath("//a[text()='Username']")).isEnabled();
-	//	Thread.sleep(2000);
-	//	System.out.println("is forgotButtonEnabled Button enabled? Ans:" + forgotButtonEnabled);
-	//}
 
 	@AfterTest
 	public void tearUp() {
--- src/hw16/Use_of_send_key_method_click_submit_and_clear.java
+++ src/hw16/Use_of_send_key_method_click_submit_and_clear.java
@@ -1,16 +1,13 @@
 package hw16;
 
-import java.sql.Driver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Use_of_get_text_method {
+public class Use_of_send_key_method_click_submit_and_clear {
 	WebDriver driver;
 
 	@BeforeTest
@@ -21,15 +18,21 @@ public class Use_of_get_text_method {
 		driver.manage().window().fullscreen();
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.amazon.com/ref=nav_logo");
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 	}
 
 	@Test(enabled = true, priority = 1)
-	public void getTextMethodTest() throws InterruptedException {
-		WebElement object = driver.findElement(By.xpath("//span[text()=\"Returns\"]"));
+	public void sendKeyMethodTest() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("books");
 		Thread.sleep(2000);
-		object.getText();
-		System.out.println("The text of the button is :" + object.getText());
+		driver.findElement(By.xpath("//input[@data-ved=\"0ahUKEwjzyfuPotr2AhVFiOAKHXbDDOQQ4dUDCA8\"]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]")).clear();
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("//input[@class=\"gLFyf gsfi\"]")).sendKeys("Mushfiqur Rahim");
+		//Thread.sleep(5000);
+		//driver.findElement(By.xpath("//input[@data-ved=\"0ahUKEwjzyfuPotr2AhVFiOAKHXbDDOQQ4dUDCA8\"]")).click();
+		//Thread.sleep(5000);
 	}
 
 	@AfterTest
